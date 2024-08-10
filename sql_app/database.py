fb0e6164f92ab769a47977a1078742a4f35fd1e3
Venkatesh-admin <87011382+Venkatesh-admin@users.noreply.github.com>
2024-08-10 14:02:01 +0530

    Update database.py

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import OperationalError
-from .models import User  # Import the User model
 
 DATABASE_URL = "sqlite:///./test.db"
 
